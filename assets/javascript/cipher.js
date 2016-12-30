de37e9ed03034508d858fcbc1e685e416fc2772f
James <jwin4740@gmail.com>
2016-12-30 08:51:15 -0800

    first

---
+++ assets/javascript/cipher.js
@@ -0,0 +1,32 @@
+ $(document).ready(function() {
+
+     // define global variables
+
+
+
+
+     //----------------START caesar code-----------------------//
+
+
+
+
+
+
+
+
+
+     //----------------END caesar code---------------------------//   
+
+     //----------------START vigenere code-----------------------//
+
+
+
+
+    
+
+
+
+
+
+     //----------------END vigenere code-------------------------//
+ });
